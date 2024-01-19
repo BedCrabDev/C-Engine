ade8f8902141b27ca5816247961941a95149d7eb
Andrei0016 <100359531+Andrei0016@users.noreply.github.com>
2024-01-19 18:10:31 +0200

    Update Render 3D.cpp

--- Render 3D/Render 3D.cpp
+++ Render 3D/Render 3D.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main() {
     Window* pWindow = new Window(640, 480);
     cameraObject camera = { 0, 0, 100 };
-    std::vector<Point3D> p = { {0, 0, 0}, {10, 0, 0}, {5, 0, 15}, {5, 10, 15} };
-    std::vector<Vertecy3D> v = { {p[0], p[1]}, {p[1], p[2]}, {p[2], p[0]}, {p[1], p[3]}, {p[2], p[3]}, {p[0], p[3]} };
+    vector<Point3D> p = { {0, 0, 0}, {10, 0, 0}, {5, 0, 15}, {5, 10, 15} };
+    vector<Vertecy3D> v = { {p[0], p[1]}, {p[1], p[2]}, {p[2], p[0]}, {p[1], p[3]}, {p[2], p[3]}, {p[0], p[3]} };
     object3D* obj = new object3D(p, v);
     perspectiveObject3D per = obj->renderPerspective(camera, 640, 480);
     for (int i = 0; i < per.vertecys.size(); ++i) {
